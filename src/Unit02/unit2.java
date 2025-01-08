ba35d4818d3ba706cb0798c0a55d2409c6443405
samruddhi deshmukh <144704279+samruddhideshmukh06@users.noreply.github.com>
2025-01-08 20:53:19 +0530

    Update unit2.java

--- src/Unit02/unit2.java
+++ src/Unit02/unit2.java
@@ -1,4 +1,22 @@
 package Unit02;
 
-public class unit2 {
+public class unit2 {public static void main(String[] args) {
+        System.out.println("Main");
+        Student student = new Student();
+        student.rollno = 1;
+        student.gen ='M';
+        student.name="GOGO";
+        System.out.println("Student"+student.name);
+        student.register();
+
+
+    }
+}
+class Student {
+    int rollno;
+    char gen;
+    String name;
+    void register() {
+        System.out.println("Register");
+    }
 }
