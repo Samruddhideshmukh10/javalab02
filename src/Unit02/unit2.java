f11ff99c253c4c81c8ca02712f661c8a99e8ed4b
samruddhi deshmukh <144704279+Samruddhideshmukh10@users.noreply.github.com>
2025-03-24 11:27:43 +0530

    Update unit2.java

--- src/Unit02/unit2.java
+++ src/Unit02/unit2.java
@@ -1,22 +1,60 @@
-package Unit02;
+public class StudentApp {
+    public static void main(String[] args) {
+        System.out.println("Program Started");
+        Learner learner1 = new Learner();
+        learner1.id = 257;
+        learner1.gender = 'F';
+        learner1.fullName = "Samruddhi";
+        learner1.bodyWeight = 30f;
+        learner1.contact = "123456789";
+        learner1.birthDate = "06-10-2005";
+        System.out.println("Student Name: " + learner1.fullName);
+        learner1.enroll();
+        learner1.courseEnroll();
 
-public class unit2 {public static void main(String[] args) {
-        System.out.println("Main");
-        Student student = new Student();
-        student.rollno = 1;
-        student.gen ='M';
-        student.name="GOGO";
-        System.out.println("Student"+student.name);
-        student.register();
-
-
+        Learner learner2 = new Learner("purva", 'f', "3-06-2005", "8793000270", 247, 88F);
+        learner2.enroll(learner2.fullName);
+        learner2.courseEnroll(learner2.id);
     }
 }
-class Student {
-    int rollno;
-    char gen;
-    String name;
-    void register() {
-        System.out.println("Register");
+
+class Learner {
+    String fullName;
+    char gender;
+    String birthDate;
+    String contact;
+    int id;
+    float bodyWeight;
+
+    public Learner(String fullName, char gender, String birthDate, String contact, int id, float bodyWeight) {
+        this.fullName = fullName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.contact = contact;
+        this.id = id;
+        this.bodyWeight = bodyWeight;
+
+        System.out.println("Name: " + fullName + ", Gender: " + gender + ", DOB: " + birthDate + ", Contact: " + contact + ", ID: " + id + ", Weight: " + bodyWeight);
+    }
+
+    public Learner() {
+        System.out.println("Default Constructor Executed");
+    }
+
+    void enroll() {
+        System.out.println("Enrollment Successful");
+    }
+
+    void enroll(String fullName) {
+        System.out.println(fullName + " has been enrolled successfully.");
+    }
+
+    void courseEnroll() {
+        System.out.println("Course Enrollment Successful");
+    }
+
+    void courseEnroll(int id) {
+        System.out.println("Learner with ID " + id + " has been enrolled in the course.");
     }
 }
+
